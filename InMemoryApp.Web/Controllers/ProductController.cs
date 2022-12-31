df316682ccbd9351d5e6f18b7cefe48d269aed04
Enes Arat <enes_arat@outlook.com>
2023-01-01 00:04:21 +0300

    Contains: Example usages on Get, Set, TryGetValue, GetOrCreate and Remove methods of IMemoryCache

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -18,6 +18,34 @@ namespace InMemoryApp.Web.Controllers
 
         public IActionResult Index()
         {
+            // Method 1 to check the existence of the related key's variance in the memory
+            if (String.IsNullOrEmpty(_memoryCache.Get<string>("Time")))
+            {
+                _memoryCache.Set<string>("Time", DateTime.Now.ToString());
+
+            }
+
+            // Method 2 to check the existence of the related key's variance in the memory  
+            if (_memoryCache.TryGetValue("Time", out string timeCache))
+            {
+                _memoryCache.Set<string>("Time", DateTime.Now.ToString());
+            }
+
+            return View();
+        }
+
+        public IActionResult GetDataFromCache()
+        {
+            // To remove data from cache according to key
+            //_memoryCache.Remove("Time");
+
+            // To get data from cache or create into cache according to key
+            ViewBag.time = _memoryCache.GetOrCreate<string>("Time", entry =>
+            {
+                return DateTime.Now.ToString();
+            });
+
+            //ViewBag.time = _memoryCache.Get<string>("Time");
             return View();
         }
     }
